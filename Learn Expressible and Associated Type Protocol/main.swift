bccee7553c9c4e470f9c173e6a2044844be87fdb
Steven Hertz <menashehertz@gmail.com>
2017-12-13 11:05:38 -0500

    Initial Commit

---
+++ Learn Expressible and Associated Type Protocol/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Learn Expressible and Associated Type Protocol
+//
+//  Created by Steven Hertz on 12/13/17.
+//  Copyright © 2017 Steven Hertz. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
